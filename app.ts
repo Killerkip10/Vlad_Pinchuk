cf38b3a9a407a762436f37e8b4edc42f1796e3fe
Killekip10 <vpinchuk2012@gmail.com>
2018-09-20 14:08:46 +0300

    update API

--- app.ts
+++ app.ts
@@ -4,20 +4,20 @@ import * as bodyParser from 'body-parser';
 
 const uuid = require('uuid/v4');
 const path = require('path');
-const fs = require('fs').promises;
+const morgan = require('morgan');
+// const fs = require('fs').promises;
 
 class User{
   constructor(
-    public readonly name: string,
-    public readonly password: string,
-    public readonly dateOfBirth: string,
-    public readonly dateOfNextNotification: string,
-    public readonly information: string,
-    public town: string,
-    public phone: string,
-    public surname: string,
-    public gender: string
-  ){}
+    public name: string,
+    public password: string,
+    public information: string,
+    public dateOfBirth: string,
+    public dateOfNextNotification: string
+  ){
+    this.dateOfBirth = new Date(this.dateOfBirth).toISOString();
+    this.dateOfNextNotification = new Date(this.dateOfNextNotification).toISOString();
+  }
   public readonly id: string = uuid();
   public readonly dateOfFirstLogin: string = new Date().toISOString();
 }
@@ -25,25 +25,17 @@ class UserContainer{
   constructor(
     private users: User[]
   ){}
-  public addUser(user: User){
+  public addUser(user: User): User{
     const newUser = new User(
       user.name,
       user.password,
-      user.dateOfBirth,
-      user.dateOfNextNotification,
       user.information,
-      user.town,
-      user.phone,
-      user.surname,
-      user.gender
+      user.dateOfBirth,
+      user.dateOfNextNotification
     );
     this.users.push(newUser);
 
-    return new Promise((resolve, reject)=>{
-      this.saveUsers()
-        .then(()=>resolve(newUser))
-        .catch(err=>reject(err))
-    });
+    return newUser;
   }
   public getUsers(): User[]{
     return this.users;
@@ -51,33 +43,32 @@ class UserContainer{
   public getUserById(id: string): User | undefined{
     return this.users.find(user=>user.id === id);
   }
-  public deleteUser(id: string){
+  public deleteUser(id: string): void{
     this.users = this.users.filter(user=>user.id !== id);
-    return this.saveUsers()
   }
-  public updateUser(updateUser: User, id: string){
-    const user = this.getUserById(id);
+  public updateUser(updateUser: User, id: string): void{
+    let user = this.getUserById(id);
 
     if(!user)
-      return new Promise(resolve=>resolve());
+      return;
 
     const {
-      town = user.town,
-      phone = user.phone,
-      surname = user.surname,
-      gender = user.gender
+      name = user.name,
+      password = user.password,
+      dateOfBirth = user.dateOfBirth,
+      dateOfNextNotification = user.dateOfNextNotification,
+      information = user.information
     } = updateUser;
 
-    user.town = town;
-    user.phone = phone;
-    user.surname = surname;
-    user.gender = gender;
-
-    return this.saveUsers();
+    user.name = name;
+    user.password = password;
+    user.dateOfBirth = new Date(dateOfBirth).toISOString();
+    user.dateOfNextNotification = new Date(dateOfNextNotification).toISOString();
+    user.information = information;
   }
-  private saveUsers(): Promise<Error>{
-    return fs.writeFile(path.join(__dirname, '../users.json'), JSON.stringify(this.users))
-  }
+  // private saveUsers(): Promise<Error>{
+  //   return fs.writeFile(path.join(__dirname, '../users.json'), JSON.stringify(this.users))
+  // }
 }
 
 const app = express();
@@ -86,6 +77,7 @@ const container = new UserContainer(require(path.join('../', 'users.json')));
 
 
 app.use(bodyParser.json());
+app.use(morgan('combined'));
 
 
 app.route('/users')
@@ -98,21 +90,17 @@ app.route('/users/:id')
     res.status(200).send(container.getUserById(req.params.id));
   })
   .put((req: Request, res: Response, next: NextFunction)=>{
-    container.updateUser(req.body, req.params.id)
-      .then(()=>res.status(204).send())
-      .catch(err=>next(err))
+    container.updateUser(req.body, req.params.id);
+    res.status(204).send();
   })
   .delete((req: Request, res: Response, next: NextFunction)=>{
-    container.deleteUser(req.params.id)
-      .then(()=>res.status(204).send())
-      .catch(err=>next(err))
+    container.deleteUser(req.params.id);
+    res.status(204).send();
   });
 
 app.route('/users/add')
   .post((req: Request, res: Response, next: NextFunction)=>{
-    container.addUser(req.body)
-      .then(user=>res.status(201).send(user))
-      .catch(err=>next(err))
+    res.status(201).send(container.addUser(req.body));
   });
 
 
